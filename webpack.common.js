f46a0516f0f2926e6e06a83c684d17d4211914dd
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-02-05 14:00:04 +0530

    feat: css dynamically added to dist

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: {
@@ -8,6 +9,9 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
         }),
     ],
     output: {
