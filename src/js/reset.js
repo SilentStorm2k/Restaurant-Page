3133432e311c356fe2ef934b17c9ddc2fa402a09
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-02-06 17:50:09 +0530

    feat: now removes all classes associated with content (for additional styling)

--- src/js/reset.js
+++ src/js/reset.js
@@ -1,5 +1,6 @@
 export default function reset () {
     const contentContainer = document.getElementById("content");
+    contentContainer.removeAttribute('class');
     while (contentContainer.firstChild)
         contentContainer.removeChild(contentContainer.firstChild);
 }
