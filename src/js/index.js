b979cfb63be6691f39131696b5e6c82a1b3044b0
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-02-06 18:15:19 +0530

    feat: added a main function call other auxillary startup functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,20 @@
 import { HomePage } from "./initialPageLoad";
 import 'normalize.css';
-import  '../css/styles.css';
-
-HomePage();
+import '../css/styles.css';
+import addFavicon from "./favicon";
+import { MenuPage } from "./menu";
 
 const homeButton = document.getElementById("home");
 const menuButton = document.getElementById("menu");
 const aboutButton = document.getElementById("about");
 
-homeButton.addEventListener('click', HomePage);
-menuButton.addEventListener('click', MenuPage);
-aboutButton.addEventListener('click', AboutPage);
+
+function init() {
+    homeButton.addEventListener('click', HomePage);
+    menuButton.addEventListener('click', MenuPage);
+    aboutButton.addEventListener('click', AboutPage);
+    addFavicon();
+    HomePage();
+}
+
+init();
