f1b52d936be5c8afa694be0dd77659de0f807308
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-02-06 13:17:26 +0530

    fix: import statement for logo image fixed

--- src/js/initialPageLoad.js
+++ src/js/initialPageLoad.js
@@ -1,9 +1,11 @@
-export function addContent () {
+import logo from '../images/logo.jpeg';
+
+export function HomePage () {
     const contentContainer = document.getElementById("content");
     const image = document.createElement("img");
     const heading = document.createElement("h2");
     const content = document.createElement("p");
-    image.src = "images/logo.jpeg";
+    image.src = logo;
     heading.textContent = "Ichiraku Ramen";
     content.textContent = "Welcome to ichiraku ramen, where you can indulge in your carnal desires to eat Naruto the 7th hokage himself";
     contentContainer.appendChild(image);
