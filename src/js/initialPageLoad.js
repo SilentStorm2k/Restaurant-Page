b66503d262445efefe4cc9e14d23bbbdb3855584
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-02-06 17:48:52 +0530

    feat: moved all element creation and assignment to separate functions

--- src/js/initialPageLoad.js
+++ src/js/initialPageLoad.js
@@ -1,14 +1,42 @@
 import logo from '../images/logo.jpeg';
+import reset from './reset';
 
 export function HomePage () {
+    reset();
     const contentContainer = document.getElementById("content");
-    const image = document.createElement("img");
+    contentContainer.classList.add('home');
+
+    const bgImage = createBGImage(contentContainer); 
+    const contentBox = createContentBox(contentContainer);
+    const heading = createHeading(contentBox);
+    const content = createContent(contentBox);
+
+    contentContainer.appendChild(contentBox);
+}
+
+function createContentBox (contentContainer) {
+    const contentBox = document.createElement('div');
+    contentContainer.appendChild(contentBox);
+    return contentBox;
+}
+
+function createHeading (container) {
     const heading = document.createElement("h2");
-    const content = document.createElement("p");
-    image.src = logo;
     heading.textContent = "Ichiraku Ramen";
+    container.appendChild(heading);
+    return heading;
+}
+
+function createContent (container) {
+    const content = document.createElement("p");
     content.textContent = "Welcome to ichiraku ramen, where you can indulge in your carnal desires to eat Naruto the 7th hokage himself";
-    contentContainer.appendChild(image);
-    contentContainer.appendChild(heading);
-    contentContainer.appendChild(content);
+    container.appendChild(content);
+    return content;
+}
+
+function createBGImage (container) {
+    const image = document.createElement("img");
+    image.src = logo;
+    container.appendChild(image);
+    return image;
 }
